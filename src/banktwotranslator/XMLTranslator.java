f11c4cdcfbbdce4d5b8ee517e5f2476080b896d7
Kasper <Kasper@10.50.137.164>
2014-12-05 14:50:00 +0100

    changed

--- src/banktwotranslator/XMLTranslator.java
+++ src/banktwotranslator/XMLTranslator.java
@@ -5,16 +5,19 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
+import com.rabbitmq.client.QueueingConsumer.Delivery;
 import java.io.IOException;
 
 /**
  *
  * @author mhck
  */
-public class BankTwoTranslator {
+public class XMLTranslator {
     private static final String BANKEXCHANGE_NAME = "cphbusiness.bankXML";
     private static final String REPLY_QUEUE = "bank_one_normalizer";
+    private static final String EXCHANGE_NAME = "translator_exchange_topic";
     private static final String QUEUE_NAME = "xml_translator_two";
+    private static final String[] TOPICS = {"expensive.*"};
 
     public static void main(String[] args) throws IOException, InterruptedException {
         ConnectionFactory factory = new ConnectionFactory();
@@ -26,7 +29,12 @@ public class BankTwoTranslator {
         Channel channelIn = connection.createChannel();
         Channel channelOut = connection.createChannel();
         channelIn.queueDeclare(QUEUE_NAME, true, false, false, null);
-
+        channelIn.exchangeDeclare(EXCHANGE_NAME, "topic");
+        
+        for(String topic: TOPICS){
+            channelIn.queueBind(QUEUE_NAME, EXCHANGE_NAME,topic);
+        }
+        
         QueueingConsumer consumer = new QueueingConsumer(channelIn);
         channelIn.basicConsume(QUEUE_NAME, true, consumer);
 //        String testMessage = "{\"ssn\":1605789787,\"loanAmount\":10.0,\"loanDuration\":360,\"rki\":false}"; //test sender besked til sig selv.
@@ -34,12 +42,12 @@ public class BankTwoTranslator {
 //        channel.basicPublish("", QUEUE_NAME, null, testMessage.getBytes()); // test
 
         while (true) {
-            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            Delivery delivery = consumer.nextDelivery();
             //channelIn.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             //System.out.println(new String(delivery.getBody()));
             String message = translateMessage(delivery);
             System.out.println(message);
-            BasicProperties probs = new BasicProperties.Builder().replyTo(REPLY_QUEUE).build();
+            BasicProperties probs = new BasicProperties.Builder().replyTo(REPLY_QUEUE).correlationId("1").build();
             channelOut.basicPublish(BANKEXCHANGE_NAME, "", probs, message.getBytes());
         }
     }
