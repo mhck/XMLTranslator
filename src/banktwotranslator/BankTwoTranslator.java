5bcee2baa2872888748c1d55734a575eb228f80e
mhck <heckmanator@gmail.com>
2014-12-05 09:49:20 +0100

    imported connection lib

--- src/banktwotranslator/BankTwoTranslator.java
+++ src/banktwotranslator/BankTwoTranslator.java
@@ -2,9 +2,8 @@ package banktwotranslator;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.QueueingConsumer;
+import dk.cphbusiness.connection.ConnectionCreator;
 import java.io.IOException;
 
 /**
@@ -17,26 +16,14 @@ public class BankTwoTranslator {
     private static final String QUEUE_NAME = "xml_translator_two";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setUsername("nicklas");
-        factory.setPassword("cph");
-        factory.setHost("datdb.cphbusiness.dk");
-
-        Connection connection = factory.newConnection();
-        Channel channelIn = connection.createChannel();
-        Channel channelOut = connection.createChannel();
+        ConnectionCreator cc = ConnectionCreator.getInstance();
+        Channel channelIn = cc.createChannel();
+        Channel channelOut = cc.createChannel();
         channelIn.queueDeclare(QUEUE_NAME, true, false, false, null);
-
         QueueingConsumer consumer = new QueueingConsumer(channelIn);
         channelIn.basicConsume(QUEUE_NAME, true, consumer);
-//        String testMessage = "{\"ssn\":1605789787,\"loanAmount\":10.0,\"loanDuration\":360,\"rki\":false}"; //test sender besked til sig selv.
-//        String testMessage = "{\"ssn\":1605789787,\"creditScore\":598,\"loanAmount\":10.0,\"loanDuration\":360}";
-//        channel.basicPublish("", QUEUE_NAME, null, testMessage.getBytes()); // test
-
         while (true) {
             QueueingConsumer.Delivery delivery = consumer.nextDelivery();
-            //channelIn.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-            //System.out.println(new String(delivery.getBody()));
             String message = translateMessage(delivery);
             System.out.println(message);
             BasicProperties probs = new BasicProperties.Builder().replyTo(REPLY_QUEUE).build();
